f0b867be9c372dead31ce196a1f7e12b8d7a0828
Jacob Turpin <jacobeturpin@gmail.com>
2025-11-11 15:23:30 -0500

    feat: set up fastapi and ws test

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.websocket("/ws/test")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received message: {data}")
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
+
 def main():
     print("Hello from agentic-ai-ivr-demo!")
 
