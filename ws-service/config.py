cd0ad260115bb6d5fae912ae435ab14d2526fc3a
jacobeturpin <jacobeturpin@gmail.com>
2025-11-14 15:01:28 -0500

    refactor: update deprecated patterns

--- ws-service/config.py
+++ ws-service/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
+        env_ignore_empty=True,
         case_sensitive=False,
         extra="ignore",
     )
