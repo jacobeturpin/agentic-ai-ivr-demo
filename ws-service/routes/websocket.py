b30fd046aad5bba3028841813bac78d58e720c8c
jacobeturpin <jacobeturpin@gmail.com>
2025-11-17 15:50:19 -0500

    feat: accept media over ws

--- ws-service/routes/websocket.py
+++ ws-service/routes/websocket.py
@@ -62,3 +62,65 @@ async def websocket_endpoint(websocket: WebSocket):
                 "error_type": type(e).__name__,
             }
         )
+
+@router.websocket('ws/media')
+async def receive_media(websocket: WebSocket):
+    """Receive media from Twilio Media Streaming"""
+    client_host = websocket.client.host if websocket.client else "unknown"
+    client_port = websocket.client.port if websocket.client else 0
+    
+    await websocket.accept()
+    logger.info(
+        "WebSocket connection established",
+        extra={
+            "client_host": client_host,
+            "client_port": client_port,
+        }
+    )
+
+    try:
+        message_count = 0
+
+        while True:
+            media = await websocket.receive_json()
+
+            if media['event'] == "connected":
+                logger.info("Connected Message received", media)
+            elif media['event'] == "start":
+                logger.info("Start Message received", media)
+            elif media['event'] == "media":
+                if not has_seen_media:
+                    logger.info("Media message", media)
+                    logger.info("Additional media messages from WebSocket are being suppressed....")
+                    has_seen_media = True
+            elif media['event'] == "closed":
+                logger.info("Closed Message received", media)
+                break
+            else:
+                logger.warning("Incorrectly formatted message was ignored", media)
+                continue
+            count += 1
+
+        logger.info("Connection closed. Received a total of {} messages".format(count))
+        websocket.close()
+
+    except WebSocketDisconnect:
+        logger.info(
+            "WebSocket client disconnected",
+            extra={
+                "client_host": client_host,
+                "client_port": client_port,
+            }
+        )
+    except Exception as e:
+        logger.error(
+            "WebSocket error occurred",
+            exc_info=True,
+            extra={
+                "client_host": client_host,
+                "error_type": type(e).__name__,
+            }
+        )
+
+
+
