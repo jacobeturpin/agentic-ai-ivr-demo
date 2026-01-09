750ac7cf51e13388759833424a935407f8bdfae4
jacobeturpin <jacobeturpin@gmail.com>
2026-01-08 20:38:15 -0500

    feat: add fastapi server

--- mock_api/main.py
+++ mock_api/main.py
@@ -1,3 +1,13 @@
+# Mock API Service
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 def main():
     print("Hello from mock-api!")
 
